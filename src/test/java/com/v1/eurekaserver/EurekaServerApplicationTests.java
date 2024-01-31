b37f528957fec2c9ad25592c9119d05a364975d4
mac <taranenkovo03@gmail.com>
2024-01-31 17:54:09 +0200

    Dockerfile add fix bugs

--- src/test/java/com/v1/eurekaserver/EurekaServerApplicationTests.java
+++ src/test/java/com/v1/eurekaserver/EurekaServerApplicationTests.java
@@ -3,9 +3,15 @@ package com.v1.eurekaserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * The type Eureka server application tests.
+ */
 @SpringBootTest
 class EurekaServerApplicationTests {
 
+    /**
+     * Context loads.
+     */
     @Test
     void contextLoads() {
     }
